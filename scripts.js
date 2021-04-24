22fb34fe7ff1d5114b3b250984e730d4f5bd11bc
Manzovince <manzovince@outlook.fr>
2021-04-24 22:04:46 +0200

    first working version with days

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+function inDays(start, end) {
+    return Math.round((end-start)/(1000*60*60*24))-1;
+}
+
 function createCal() {
     var bDay = document.getElementById("bDay").value;
     var bMonth = document.getElementById("bMonth").value;
@@ -11,20 +15,52 @@ function createCal() {
 
     // Init dates
     var birthday = new Date(bYear, bMonth, bDay);
+    var end = new Date(Number(bYear)+40, bMonth, bDay);
     var now = new Date();
 
-    var msecPassed = now - birthday;
-    var daysPassed = Math.round((now-birthday)/(1000*60*60*24));
-    var weeksPassed = Math.round(daysPassed/7);
-
-    console.log(msecPassed, daysPassed, weeksPassed);
+    //var msecPassed = now - birthday;
+    //var daysPassed = Math.round((now-birthday)/(1000*60*60*24))-1;
+    //var weeksPassed = Math.round(daysPassed/7);
 
     var html = "";
     var age = 0;
 
-    for (i = 0; i < daysPassed; i++) {
-        html += `<div class="cell">-</div>`
+    var t = new Date(birthday);
+
+    while (t <= end){
+        // Happy birthday
+        if (t.getFullYear() == bYear && t.getMonth() == 9 && t.getDate() == 14) {
+            html += `<div class="cell hbd"><span class="tooltip">`+t.getDate()+`-`+(t.getMonth()+1)+`-`+t.getFullYear()+` (`+age+`yo 🥳)</span></div>`;
+        }
+        console.log(t == now);
+        if (t == now) {
+            console.log("Hello");
+            html += `<div class="cell now"><span class="tooltip">`+t.getDate()+`-`+(t.getMonth()+1)+`-`+t.getFullYear()+`</span></div>`;
+        }
+        else {
+            html += `<div class="cell"><span class="tooltip">`+t.getDate()+`-`+(t.getMonth()+1)+`-`+t.getFullYear()+`</span></div>`;
+        }
+        var newDate = t.setDate(t.getDate() + 1);
+        if (t.getMonth() == 9 && t.getDate() == 14) {
+            bYear++;
+            age++;
+        }
+        t = new Date(newDate);
     }
-    
-    document.getElementById("calendar").innerHTML += html;
+
+//    for (var t = birthday; t < end; t.setDate(t.getDate() + 1)) {
+//        console.log(t, new Date(t));
+//        html += `<div class="cell"><span class="tooltip">`+t.getDate()+`-`+(t.getMonth()+1)+`-`+t.getFullYear()+`</span></div>`;
+//    }
+
+//    for (i = 0; i < daysPassed; i++) {
+//        html += `<div class="cell"><span class="tooltip">`+start.getDate()+`-`+(start.getMonth()+1)+`-`+start.getFullYear()+`(`+i+`)`+`</span></div>`;
+//        if (i % 53 == 0) {
+//            html += `<div class="cell hbd"><span class="tooltip">`+start.getDate()+`-`+(start.getMonth()+1)+`-`+start.getFullYear()+`(`+i+`)`+`</span>`+age+`</div>`;
+//            age++;
+//        }
+//        start.setDate(start.getDate()+7);
+//    }
+
+    document.getElementById("calendar").innerHTML = html;
 }
